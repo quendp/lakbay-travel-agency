62b690400929f390dbe8df7dca0d553dfa79d78f
Quen DP <delpilarquen@gmail.com>
2022-11-29 04:05:30 +0800

    Add index.html header menu items

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,3 @@
 function hShowNav(){
-    document.getElementsByClassName("h-header-wrapper")[0].classList.toggle("h-active");
+    document.getElementById("h-header-wrapper").classList.toggle("h-active");
 }
