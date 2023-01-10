f7294ce4a6deac23b71bd3ebbf8fb60e4b8ed156
Quen DP <delpilarquen@gmail.com>
2023-01-10 15:15:46 +0800

    Add parallax-scroll.js and remove scroll easing effect

---
+++ sources/scripts/parallax-scroll.js
@@ -0,0 +1,17 @@
+const parallaxElement = document.querySelectorAll(".parallax-scroll");
+window.addEventListener("scroll", () => {
+    const viewportHeight = document.getElementById("mHeroSection").clientHeight;
+    for (let i of parallaxElement) {
+        const elementHeight = i.clientHeight;
+        const elementTop = i.getBoundingClientRect().top;
+        const elementCentered = (viewportHeight - elementHeight)/2;
+        const scrolled = elementTop - elementCentered;
+        if(elementTop <= viewportHeight && elementTop >= elementCentered && i.dataset.direction == "left") {
+            i.style.transform = `translate(${-scrolled}px, 0)`;
+        } else if (elementTop <= viewportHeight && elementTop >= elementCentered) {
+            i.style.transform = `translate(${scrolled}px, 0)`;
+        } else {
+            i.style.transform = `translate(0, 0)`;
+        }
+    };
+});
--- sources/scripts/scroll-ease.js
+++
@@ -1,4 +0,0 @@
-let scroller = new SmoothScroll({
-    target: document.querySelector("main"),
-    scrollEase: 0.08,
-  });
