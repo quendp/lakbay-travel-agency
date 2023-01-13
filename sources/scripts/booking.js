6a86a62bb012f7370c3bc7526dbe2975e0a6159a
Quen DP <delpilarquen@gmail.com>
2023-01-13 10:13:24 +0800

    Change data-depth of html under pages directory from 2 to 1

---
+++ sources/scripts/booking.js
@@ -0,0 +1,128 @@
+const mCategories = [
+    {
+        category : "Cosmopolitan Lights",
+        categoryTitle : "Visit the Lively <br> and Colorful Cities" ,
+        description : "A trip to Star City Amusement Park, Manila Ocean Park, Chinatown, and Solaire Resort and Casino in Manila, and Mall of Asia and Cultural Center of the Philippines in Pasay.",
+        buttonUrl : "../../../pages/categories/cosmopolitan-lights/cosmopolitan-lights.html",
+        backgroundUrl : "../../../sources/videos/city.mp4",
+        poster : "../../../sources/images/hero-section-thumbnails/city-poster.jpg",
+        accent: "var(--clr-accent-lights)",
+        accentLight: "var(--clr-accent-lights-100",
+        circleImage : [
+            "../../../sources/images/cosmopolitan-lights/star-city.webp", 
+            "../../../sources/images/cosmopolitan-lights/chinatown.jpg",
+            "../../../sources/images/cosmopolitan-lights/mall-of-asia.jpg",
+            "../../../sources/images/cosmopolitan-lights/manila-ocean-park.jpg",
+            "../../../sources/images/cosmopolitan-lights/solaire.jpg",
+            "../../../sources/images/cosmopolitan-lights/ccp.webp"
+        ],
+        circleText : [
+            "Star City Amusement Park in Pasay",
+            "Chinatown in Binondo Manila",
+            "Mall of Asia in Pasay City",
+            "Manila Ocean Park",
+            "Solaire Resort & Casino Manila",
+            "Cultural Center of the Philippines in Pasay City"
+        ]
+    }, {
+        category : "Dive Under Water",
+        categoryTitle : "Discover the Wonders <br> of the Deep Sea" ,
+        description : "A trip towards places such as the Bantayan Island in Cebu, Boracay Beach in Aklan, El Nido in Palawan, Palaui Island in Santa Ana, Panglao Island in Bohol, and Siargao Island.",
+        buttonUrl : "../../../pages/categories/dive-under-water/dive-under-water.html",
+        backgroundUrl : "../../../sources/videos/water.mp4",
+        poster : "../../../sources/images/hero-section-thumbnails/water-poster.jpg",
+        accent: "var(--clr-accent-water)",
+        accentLight: "var(--clr-accent-water-100",
+        circleImage : [
+            "../../../sources/images/dive-under-water/bantayan.jpg", 
+            "../../../sources/images/dive-under-water/boracay.jpg",
+            "../../../sources/images/dive-under-water/el-nido-eibner-saliba-3T9dDY0WqDI-unsplash.jpg",
+            "../../../sources/images/dive-under-water/palaui.jpg",
+            "../../../sources/images/dive-under-water/panglao.jpg",
+            "../../../sources/images/dive-under-water/siargao.jpg"
+        ],
+        circleText : [
+            "Bantayan Island in Cebu",
+            "Boracay Beach in Aklan",
+            "El Nido in Palawan",
+            "Palaui Island in Santa Ana",
+            "Panglao Island in Bohol",
+            "Siargao island"
+        ]
+    }, {
+        category : "Explore the Summit",
+        categoryTitle : "Look at the Sky on <br> Top of Mountain Peaks" ,
+        description : "Visit beautiful mountains such as Mt. Apo in Davao City, Mt. Guiting Guiting in Sibuyan, Mt. Pulag and Mt. Itogon in Benguet, Mt. Pinatubo in Zambales, and Mt. Ugo in Nueva Viscaya.",
+        buttonUrl : "../../../pages/categories/explore-the-summit/explore-the-summit.html",
+        backgroundUrl : "../../../sources/videos/mountain.mp4",
+        poster : "../../../sources/images/hero-section-thumbnails/mountain-poster.jpg",
+        accent: "var(--clr-accent-summit)",
+        accentLight: "var(--clr-accent-summit-100",
+        circleImage : [
+            "../../../sources/images/explore-the-summit/apo.jpg", 
+            "../../../sources/images/explore-the-summit/guiting-guiting.jpg",
+            "../../../sources/images/explore-the-summit/mount-pulag-joanne-caselyn-kCGhXLU32Bg-unsplash.jpg",
+            "../../../sources/images/explore-the-summit/pinatubo.jpg",
+            "../../../sources/images/explore-the-summit/ugo.jpg",
+            "../../../sources/images/explore-the-summit/ulap.jpg"
+        ],
+        circleText : [
+            "Mt. Apo in Davao City",
+            "Mt. Guiting-Guiting National Park in Sibuyan Island",
+            "Mt. Pulag in Benguet",
+            "Mt. Pinatubo in Zambales ",
+            "Mt. Ugo in Nueva Viscaya",
+            "Mt. Ulap in Itogon, Benguet"
+        ]
+    }, {
+        category : "Look Back in History",
+        categoryTitle : "Embrace the Great <br> Remnants of the Past" ,
+        description : "Visit Historical Sites such as Calle Crisologo in Vigan, Fort Santiago in Intramuros, Magellan's Cross in Mactan, National Museum and Rizal Park in Manila, and Rizal Shrine in Dapitan.",
+        buttonUrl : "../../../pages/categories/look-back-in-history/look-back-in-history.html",
+        backgroundUrl : "../../../sources/videos/history.mp4",
+        poster : "../../../sources/images/hero-section-thumbnails/history-poster.jpg",
+        accent: "var(--clr-accent-history)",
+        accentLight: "var(--clr-accent-history-100",
+        circleImage : [
+            "../../../sources/images/look-back-in-history/calle-crisologo-593843_1920.jpg", 
+            "../../../sources/images/look-back-in-history/fort-santiago.jpg",
+            "../../../sources/images/look-back-in-history/mactan.jpg",
+            "../../../sources/images/look-back-in-history/museum.jpg",
+            "../../../sources/images/look-back-in-history/rizal.jpg",
+            "../../../sources/images/look-back-in-history/rizal-shrine.jpg"
+        ],
+        circleText : [
+            "Calle Crisologo in Vigan, Ilocos Sur",
+            "Fort Santiago, Intramuros, Manila",
+            "Magellan's Cross in Mactan, Cebu",
+            "National Museum of Natural History in Manila",
+            "Rizal park in Manila",
+            "Rizal Shrine in Dapitan, Zamboanga del Norte"
+        ]
+    }, {
+        category : "Nature and Culture",
+        categoryTitle : "Feel the Beauty of <br> the Untouched Lands" ,
+        description : "Visit the Chocolate Hills in Bohol, Barracuda Lake in Coron, Banaue Rice Terraces in Ifugao, Puerto Galera in Mindoro, Tagaytay in Cavite, and Masungi Georeserve in Tanay.",
+        buttonUrl : "../../../pages/categories/nature-and-culture/nature-and-culture.html",
+        backgroundUrl : "../../../sources/videos/nature.mp4",
+        poster : "../../../sources/images/hero-section-thumbnails/nature-poster.jpg",
+        accent: "var(--clr-accent-culture)",
+        accentLight: "var(--clr-accent-culture-100",
+        circleImage : [
+            "../../../sources/images/nature-and-culture/bohol.jpg", 
+            "../../../sources/images/nature-and-culture/coron.jpg",
+            "../../../sources/images/nature-and-culture/ifugao-ar-2FsniOcLPj8-unsplash.jpg",
+            "../../../sources/images/nature-and-culture/puerto-galera.jpg",
+            "../../../sources/images/nature-and-culture/tagaytay.jpg",
+            "../../../sources/images/nature-and-culture/tanay.jpg"
+        ],
+        circleText : [
+            "Chocolate Hills in Bohol",
+            "Barracuda Lake in Coron Island, Palawan Province",
+            "Banaue Rice Terraces in Ifugao",
+            "Puerto Galera in Mindoro oriental",
+            "Tagaytay in Cavite",
+            "Masungi Georeserve in Tanay, Rizal"
+        ]
+    }
+];
